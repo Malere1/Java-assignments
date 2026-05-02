522833f14eb1bdbbd57336c82634e5c6a82f0c44
Malere1 <adamudivine9@email.com>
2026-05-02 20:35:42 +0100

    add new files

---
+++ CLASS  WORK/Array.java
@@ -0,0 +1,18 @@
+public class Array{
+
+//    public static void printArrayItems(int[] array) {
+//        for (int index = 0; index < array.length; index++) {
+//            System.out.println(array[index]);
+//        }
+//    }
+
+    public static void main(String[] args) {
+//        printArrayItems(new int[]{2, 3, 4, 5, 6});
+          
+          int[] aisosa = {1, 2, 3, 4, 5, 6, 7, 8};
+
+          for (int i = 0; i < aisosa.length; i++){
+               System.out.println(aisosa[i]);
+          }
+    }
+}
